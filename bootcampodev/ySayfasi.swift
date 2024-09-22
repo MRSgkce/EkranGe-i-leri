78d93c773dbfd1dd3363875da5e906c234824bb6
MRSgkce <mursidegokce985@gmail.com>
2024-09-22 17:13:18 +0300

    Ekranlar basti şekilde tasarlandı ve 5 ekran çeşitli butonlarla ekran arası geiş sağlandı.

---
+++ bootcampodev/ySayfasi.swift
@@ -0,0 +1,21 @@
+//
+//  ySayfasi.swift
+//  bootcampodev
+//
+//  Created by Mürşide Gökçe on 22.09.2024.
+//
+
+import UIKit
+
+class ySayfasi: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func gerim(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
